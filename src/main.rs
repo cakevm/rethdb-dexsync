028e370733f2e46adefd0a1302833191387537bb
cakevm <cakevm@proton.me>
2024-09-24 11:00:24 +0000

    Read pair details

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use alloy::primitives::{address, b256};
+use alloy::primitives::{address, b256, U32};
 use alloy_sol_types::private::primitives::U160;
 use alloy_sol_types::SolValue;
 use eyre::eyre;
@@ -10,6 +10,21 @@ use reth_provider::{providers::StaticFileProvider, AccountReader, ProviderFactor
 use std::path::Path;
 
 const ALL_PAIRS_SLOT: B256 = b256!("0000000000000000000000000000000000000000000000000000000000000003");
+
+const PAIR_TOKEN0: B256 = b256!("0000000000000000000000000000000000000000000000000000000000000006");
+const PAIR_TOKEN1: B256 = b256!("0000000000000000000000000000000000000000000000000000000000000007");
+const PAIR_RESERVE: B256 = b256!("0000000000000000000000000000000000000000000000000000000000000008");
+
+#[allow(dead_code)]
+#[derive(Debug)]
+struct Univ2Pair {
+    pub address: Address,
+    pub token0: Address,
+    pub token1: Address,
+    pub block_timestamp_last: U32,
+    pub reserve0: U160,
+    pub reserve1: U160,
+}
 
 fn main() -> eyre::Result<()> {
     let db_path = std::env::var("RETH_DB_PATH")?;
@@ -33,7 +48,7 @@ fn main() -> eyre::Result<()> {
     Ok(())
 }
 
-fn read_univ2_pairs<T: StateProvider + AccountReader>(provider: T) -> eyre::Result<Vec<Address>> {
+fn read_univ2_pairs<T: StateProvider + AccountReader>(provider: T) -> eyre::Result<Vec<Univ2Pair>> {
     let univ2_factory = address!("5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f");
     let all_pairs_start = keccak256(ALL_PAIRS_SLOT.abi_encode());
     let pairs_length = match provider.storage(univ2_factory, ALL_PAIRS_SLOT)? {
@@ -49,7 +64,8 @@ fn read_univ2_pairs<T: StateProvider + AccountReader>(provider: T) -> eyre::Resu
     let mut pairs = Vec::with_capacity(pairs_length as usize);
     for i in 0..pairs_length {
         if let Some(pair_address) = read_array_item_address(&provider, univ2_factory, all_pairs_start, i)? {
-            pairs.push(pair_address);
+            let pair = read_pair(&provider, pair_address)?;
+            pairs.push(pair);
         }
     }
 
@@ -71,4 +87,38 @@ fn read_array_item_address<T: StateProvider + AccountReader>(
         },
         Err(e) => Err(eyre!(e)),
     }
+}
+
+fn read_pair<T: StateProvider + AccountReader>(provider: T, pair_address: Address) -> eyre::Result<Univ2Pair> {
+    let token0 = match provider.storage(pair_address, PAIR_TOKEN0) {
+        Ok(storage_value) => match storage_value {
+            None => return Err(eyre!("STORAGE_SLOT_NOT_FOUND token0, {:#?}", pair_address)),
+            Some(value) => Address::from(U160::from(value)),
+        },
+        Err(e) => return Err(eyre!(e)),
+    };
+
+    let token1 = match provider.storage(pair_address, PAIR_TOKEN1) {
+        Ok(storage_value) => match storage_value {
+            None => return Err(eyre!("STORAGE_SLOT_NOT_FOUND token1, {:#?}", pair_address)),
+            Some(value) => Address::from(U160::from(value)),
+        },
+        Err(e) => return Err(eyre!(e)),
+    };
+
+    let (block_timestamp_last, reserve1, reserve0) = match provider.storage(pair_address, PAIR_RESERVE) {
+        Ok(storage_value) => match storage_value {
+            None => (U32::ZERO, U160::ZERO, U160::ZERO), // pair not initialized
+            Some(value) => {
+                let bytes = value.to_be_bytes_vec();
+                let block_timestamp_last = U32::from_be_slice(&bytes[0..4]);
+                let reserve1 = U160::from_be_slice(&bytes[4..18]);
+                let reserve0 = U160::from_be_slice(&bytes[18..32]);
+                (block_timestamp_last, reserve1, reserve0)
+            }
+        },
+        Err(e) => return Err(eyre!(e)),
+    };
+
+    Ok(Univ2Pair { address: pair_address, token0, token1, block_timestamp_last, reserve0, reserve1 })
 }
